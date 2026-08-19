fa91de3b39f7a70fa7f9021c69dc01252dbfb41f
Xinru Lin <xinrulin@berkeley.edu>
2026-08-19 14:28:01 -0700

    fixed the function for good

--- newton.py
+++ newton.py
@@ -1,36 +1,44 @@
 def f_x(x):
-    return x**2
+    return x**3 + (2 * x**2) + 1
 
-def derivative_f_x_first(f_x, x, epsilon):
-    return (f_x(x+epsilon) - f_x(x))/ epsilon
 
-def derivative_f_x_second(f_x, x, epsilon):
-    if(callable(f_x)):
-        return (f_x(x+epsilon) - f_x(x))/ epsilon
+def derivative(func, x, epsilon=0.001):
+    if callable(func):
+        return (func(x + epsilon) - func(x)) / epsilon
     else:
         return 0
 
 
-def newtons_method(f_x, x_init, epsilon):
-    curr_val = x_init
-    next_val = x_init
-    difference = 0
+def derivative_second(func, x, epsilon=0.001):
+    if callable(func):
+        return (
+            derivative(func, x + epsilon, epsilon) - derivative(func, x, epsilon)
+        ) / epsilon
+    else:
+        return 0
+
+
+def newtons_method(func, x, epsilon=0.001):
+    """
+    Parameters:
+        f_x - the function you want to optimize
+        x_init - the intial x0 value
+        epsilon - the error difference before accepting the value as the optimized output
+    Returns the value that optimizes a binomial
+    """
+
+    curr_val = x
+    next_val = float("-inf")
+    difference = float("inf")
     # while the difference is small
-    while (difference > epsilon) :
-        first_derivative = derivative_f_x_first(f_x, x, epsilon)
-        second_derivative = derivative_f_x_first(first_derivative, x, epsilon)
+    while difference > epsilon:
+        first_derivative = derivative(func, curr_val, epsilon)
+        second_derivative = derivative_second(func, curr_val, epsilon)
 
-        next_val = curr_val - (first_derivative(curr_val)/second_derivative(curr_val))
-        difference = math.abs(next_val-curr_val)
+        next_val = curr_val - (first_derivative / second_derivative)
+        difference = abs(next_val - curr_val)
         curr_val = next_val
-    return curr_val
+        next_val = float("-inf")
+        # print(type(curr_val))
 
-if __name__ == '__main__':
-    epsilon = 0.1 #not sure what this should be
-    x_init = 2;
-    # first_der = derivative_f_x_first(f_x, x_init, epsilon)
-    # print("first_der", first_der)
-    # second_der = derivative_f_x_second(first_der, x_init, epsilon)
-    # print("second_der", second_der)
-    print(newtons_method(x_init, f_x, epsilon))
-    
+    return curr_val
