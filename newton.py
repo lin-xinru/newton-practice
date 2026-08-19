6de97b8008fdf67ca595082d8a5b23e2832fca15
Xinru Lin <xinrulin@berkeley.edu>
2026-08-19 16:06:02 -0700

    updated to reduce the number of var and remove issues with new test case

--- newton.py
+++ newton.py
@@ -2,14 +2,14 @@ def f_x(x):
     return ((x**4)/4)-x**3-x# x**3 + (2 * x**2) + 1
 
 
-def derivative(func, x, epsilon=0.001):
+def derivative(func, x, epsilon=1e-6):
     if callable(func):
         return (func(x + epsilon) - func(x)) / epsilon
     else:
         return 0
 
 
-def derivative_second(func, x, epsilon=0.001):
+def derivative_second(func, x, epsilon=1e-6):
     if callable(func):
         return (
             derivative(func, x + epsilon, epsilon) - derivative(func, x, epsilon)
@@ -18,7 +18,12 @@ def derivative_second(func, x, epsilon=0.001):
         return 0
 
 
-def newtons_method(func, x, epsilon=0.001):
+def newtons_method(func, x, epsilon=1e-6):
+    if not callable(func):
+        raise TypeError('the first argument must be a function')
+
+    if not isinstance(x, float):
+        raise TypeError('x must be an numeric')
     """
     Parameters:
         f_x - the function you want to optimize
@@ -31,11 +36,12 @@ def newtons_method(func, x, epsilon=0.001):
     next_val = x - (derivative(func, x, epsilon) / derivative_second(func, x, epsilon))
     # while the difference is large
     while abs(next_val-curr_val) > epsilon:
+        curr_val = next_val
+        
         first_derivative = derivative(func, curr_val, epsilon)
         second_derivative = derivative_second(func, curr_val, epsilon)
 
         next_val = curr_val - (first_derivative / second_derivative)
-        curr_val = next_val
 
     return {'new x': curr_val, 'value': func(curr_val)}
 
